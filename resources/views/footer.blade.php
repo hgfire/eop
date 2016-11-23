8420641d2facb05b6406fe57871d835e636c583c
hg <kethistyle@gmail.com>
2016-11-23 09:35:25 +0100

    Laravel update

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,73 @@
+
+<div class="templatemo-footer" >
+    <div class="container">
+        <div class="row">
+            <div class="text-center">
+
+                <div class="footer_container">
+                    <ul class="list-inline">
+                        <li>
+                            <a href="#">
+                                <span class="social-icon-fb"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="social-icon-rss"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="social-icon-twitter"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="social-icon-linkedin"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="social-icon-dribbble"></span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="height30"></div>
+                    <a class="btn btn-lg btn-orange" href="#" role="button" id="btn-back-to-top">Back To Top</a>
+                    <div class="height30"></div>
+                </div>
+                <div class="footer_bottom_content">
+                    <span>Copyright © 2084 <a href="#">Your Company Name</a></span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js"  type="text/javascript"></script>
+<script src="js/stickUp.min.js"  type="text/javascript"></script>
+<script src="js/colorbox/jquery.colorbox-min.js"  type="text/javascript"></script>
+<script src="js/templatemo_script.js"  type="text/javascript"></script>
+<!-- templatemo 395 urbanic -->
+
+
+ <!-- Angular Material requires Angular.js Libraries -->
+          <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
+          <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-animate.min.js"></script>
+          <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-aria.min.js"></script>
+          <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-messages.min.js"></script>
+
+          <!-- Angular Material Library -->
+          <script src="http://ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.js"></script>
+
+          <!-- Your application bootstrap  -->
+          <script type="text/javascript">
+            /**
+             * You must include the dependency on 'ngMaterial'
+             */
+            angular.module('BlankApp', ['ngMaterial']);
+          </script>
+</body>
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>EOP</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <!--
+
+    Urbanic Template
+
+    http://www.templatemo.com/tm-395-urbanic
+
+    -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/logoEOP1.ico">
+
+    <!-- Google Web Font Embed -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="js/colorbox/colorbox.css" rel="stylesheet" type="text/css">
+    <link href="css/templatemo_style.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="templatemo-top-bar" id="templatemo-top">
+    <div class="container">
+        <div class="subheader">
+            <div id="phone" class="pull-left">
+                <img src="images/phone.png" alt="phone">
+                090-080-0110
+            </div>
+            <div id="email" class="pull-right">
+                <img src="images/email.png" alt="email">
+                info@eop.com
+            </div>
+        </div>
+    </div>
+</div>
+<div style="top: 0px; position: relative;" class="templatemo-top-menu stuckMenu">
+    <div class="container">
+        <!-- Static navbar -->
+        <div class="navbar navbar-default" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand"><img src="images/templatemo_logo.png" alt="Urbanic Template" title="Urbanic Template"></a>
+                </div>
+                <div class="navbar-collapse collapse" id="templatemo-nav-bar">
+                    <ul class="nav navbar-nav navbar-right" style="margin-top: 40px;">
+                        <li class="active"><a href="#templatemo-top">Accueil</a></li>
+                        <li class=""><a href="#templatemo-services">Nos services</a></li>
+                        <li class=""><a href="#templatemo-partners">Nos partenaires</a></li>
+                        <li><a href="#templatemo-team">L'équipe</a></li>
+                        <li><a href="#templatemo-inscription">Developpeurs</a></li>
+                        <li><a href="#templatemo-contact">Contacts</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div><!--/.container-fluid -->
+        </div><!--/.navbar -->
+    </div> <!-- /container -->
+</div>
